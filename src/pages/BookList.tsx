912553a6257a36669c23f29d9a2ecd725332c343
Esdras Albino <esdrashenrique475@gmail.com>
2024-11-09 00:21:15 -0300

    add input and logic to service call

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -1,65 +1,67 @@
 // src/pages/BookList.js
 import {
-    Card,
-    CardContent,
-    Container,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  import { useEffect, useState } from "react";
+  Card,
+  CardContent,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 import { BookClient } from "../services/books/books";
 import { fetchBooks } from "../services/books/getBooks";
-  //import { Link } from "react-router-dom";
-  
-  const BookList = () => {
-    const [books, setBooks] = useState<BookClient[]>();
-    const [query, setQuery] = useState("Harry Potter");
-  
-    useEffect(() => {
-      const getBooks = async () => {
-        const data = await fetchBooks(query);
-        const bookList = data.map((item) => ({
-          id: item.id,
-          title: item.volumeInfo.title,
-          author: item.volumeInfo.authors?.join(", "),
-          genre: item.volumeInfo.categories?.[0],
-          rating: item.volumeInfo.averageRating,
-        }));
-        setBooks(bookList);
-      };
-      getBooks();
-    }, [query]);
-  
-    return (
-      <Container
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <h1>Lista de Livros</h1>
-        <div>
-          {books?.map((book) => (
-            <Card key={book.id} style={{ margin: "1rem" }}>
-              {/* <Link to={`/books/${book.id}`}> */}
-                <CardContent>
-                  <Typography variant="h6">{book.title}</Typography>
-                  <Typography variant="subtitle1">
-                    Autor: {book.author}
-                  </Typography>
-                  <Typography variant="body2">Gênero: {book.genre}</Typography>
-                  <Typography variant="body2">
-                    Avaliação: {book.rating}
-                  </Typography>
-                </CardContent>
-              {/* </Link> */}
-            </Card>
-          ))}
-        </div>
-      </Container>
-    );
-  };
-  
-  export default BookList;
-  
+//import { Link } from "react-router-dom";
+
+const BookList = () => {
+  const [books, setBooks] = useState<BookClient[]>();
+  const [query, setQuery] = useState("Harry Potter");
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const data = await fetchBooks(query);
+      const bookList = data.map((item) => ({
+        id: item.id,
+        title: item.volumeInfo.title,
+        author: item.volumeInfo.authors?.join(", "),
+        genre: item.volumeInfo.categories?.[0],
+        rating: item.volumeInfo.averageRating,
+      }));
+      setBooks(bookList);
+    };
+    getBooks();
+  }, [query]);
+
+  return (
+    <Container
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Lista de Livros</h1>
+      <TextField
+        label="Buscar Livro"
+        variant="outlined"
+        fullWidth
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div>
+        {books?.map((book) => (
+          <Card key={book.id} style={{ margin: "1rem" }}>
+            {/* <Link to={`/books/${book.id}`}> */}
+            <CardContent>
+              <Typography variant="h6">{book.title}</Typography>
+              <Typography variant="subtitle1">Autor: {book.author}</Typography>
+              <Typography variant="body2">Gênero: {book.genre}</Typography>
+              <Typography variant="body2">Avaliação: {book.rating}</Typography>
+            </CardContent>
+            {/* </Link> */}
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default BookList;
