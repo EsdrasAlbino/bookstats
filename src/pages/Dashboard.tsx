df6d75c7d6568f1ce427fb10a5d412a3ca141ad0
Esdras Albino <esdrashenrique475@gmail.com>
2024-11-10 15:54:07 -0300

    fix multi render dashboard before user write search

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import {
   Bar,
   BarChart,
@@ -17,43 +18,60 @@ interface DashboardProps {
 }
 
 export const Dashboard = ({ genreData, reviewTrendData }: DashboardProps) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true);
+    }, 1000);
+  }, [genreData, reviewTrendData]);
+  
   return (
     <div
       style={{ display: "flex", alignContent: "center", alignItems: "center" }}
     >
-      <div>
-        <Typography variant="caption">Distribuição de Avaliações por Gênero de Livro</Typography>
-        <BarChart
-          width={600}
-          height={300}
-          data={genreData}
-          margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="genre" tick={false} />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="reviews" fill="#8884d8" />
-        </BarChart>
-      </div>
+      {!show && <Typography variant="h6">Carregando metricas...</Typography>}
+      {show && (
+        <>
+          <div>
+            <Typography variant="caption">
+              Distribuição de Avaliações por Gênero de Livro
+            </Typography>
+            <BarChart
+              width={600}
+              height={300}
+              data={genreData}
+              margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="genre" tick={false} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="reviews" fill="#8884d8" />
+            </BarChart>
+          </div>
 
-      <div>
-        <Typography variant="caption">Evolução Média de Avaliações ao Longo do Tempo</Typography>
-        <LineChart
-          width={600}
-          height={300}
-          data={reviewTrendData}
-          margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis domain={[0, 1, 2, 3, 4, 5]} />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="averageReview" stroke="#82ca9d" />
-        </LineChart>
-      </div>
+          <div>
+            <Typography variant="caption">
+              Evolução Média de Avaliações ao Longo do Tempo
+            </Typography>
+            <LineChart
+              width={600}
+              height={300}
+              data={reviewTrendData}
+              margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis domain={[0, 1, 2, 3, 4, 5]} />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="averageReview" stroke="#82ca9d" />
+            </LineChart>
+          </div>
+        </>
+      )}
     </div>
   );
 };
