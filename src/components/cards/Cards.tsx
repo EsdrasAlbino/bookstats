55a7ce5ceb15c9a0154f51cdf9eca3f3284082d6
Esdras Albino <esdrashenrique475@gmail.com>
2024-11-10 16:48:21 -0300

    fix case props AUTHOR and GENRER undefined

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
-import Star from '@mui/icons-material/Star';
-import StarHalf from '@mui/icons-material/StarHalf';
-import StarOutline from '@mui/icons-material/StarOutline';
+import React from "react";
+import { Card, CardContent, Typography } from "@mui/material";
+import Star from "@mui/icons-material/Star";
+import StarHalf from "@mui/icons-material/StarHalf";
+import StarOutline from "@mui/icons-material/StarOutline";
 
 interface Book {
   id: string;
@@ -17,14 +17,11 @@ interface CardsProps {
 }
 
 const Cards: React.FC<CardsProps> = ({ book }) => {
-
   const starRender = () => {
-    
     const stars = [];
     const fullStars = Math.floor(book.rating || 0);
     const hasHalfStar = (book.rating || 0) % 1 !== 0;
     const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
-    console.log("ranting and hasHalfStar", (book.rating || 0), hasHalfStar);
 
     for (let i = 0; i < fullStars; i++) {
       stars.push(<Star key={`full-${i}`} />);
@@ -41,12 +38,17 @@ const Cards: React.FC<CardsProps> = ({ book }) => {
     return stars;
   };
 
+
   return (
-    <Card style={{width:300}}>
+    <Card style={{ width: 300 }}>
       <CardContent>
         <Typography variant="h6">{book.title}</Typography>
-        <Typography variant="subtitle1">Autor: {book.author}</Typography>
-        <Typography variant="subtitle2">Gênero: {book.genre}</Typography>
+        {book?.author && (
+          <Typography variant="subtitle1">Autor: {book.author}</Typography>
+        )}
+        {book?.genre && (
+          <Typography variant="subtitle2">Gênero: {book.genre}</Typography>
+        )}
         {starRender().map((star) => star)}
       </CardContent>
     </Card>
