613feadcf47e1036fdb4f519e32c026ce0d09eba
Esdras Albino <esdrashenrique475@gmail.com>
2024-11-11 15:09:43 -0300

    request api

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchBook } from "../services/books/getBook";
 
 interface BookDetailsProps {
   title: string;
@@ -9,17 +11,24 @@ interface BookDetailsProps {
 
 export const BookDetails = () => {
   const [book, setBook] = useState();
-  
-  useEffect(() => {}, []);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const data = await fetchBook(id as string);
+      //setBook(data?.items);
+      console.log("data", data);
+    };
+  }, []);
 
   return (
     <div>
-      <h1>{book.title}</h1>
+  {/*     <h1>{book.title}</h1>
       <h2>{book.author}</h2>
       <p>
         <strong>Published Date:</strong> {book.publishedDate}
       </p>
-      <p>{book.description}</p>
+      <p>{book.description}</p> */}
     </div>
   );
 };
